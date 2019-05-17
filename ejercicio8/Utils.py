124f432ede3b94466416aae637c70c378da49b8c
nico-rodriguez <nico-rodriguez@users.noreply.github.com>
2019-05-17 14:57:40 -0300

    Added derivatives of Sigmoid and Hyperbolic Tangent.

--- ejercicio8/Utils.py
+++ ejercicio8/Utils.py
@@ -14,3 +14,18 @@ def tanh(arr):
 	e_menos_x = np.exp(-arr)
 	e_mas_x = np.exp(arr)
 	return (e_mas_x + e_menos_x) / (e_mas_x - e_menos_x)
+
+
+"""
+Aplicar la derivada de la función Sigmoide a cada elemento de un arreglo numpy.
+"""
+def d_sigmoid(arr):
+	s = sigmoid(arr)
+	return s * (1 - s)
+
+
+"""
+Aplicar la derivada de la función tangente hiperbolica a cada elemento de un arreglo numpy.
+"""
+def d_tanh(arr):
+	return 1 - (tanh(arr) ** 2)
