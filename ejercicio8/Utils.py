48c89bad28f7d3f2e13bf996ab42ad5a2076c508
nico-rodriguez <nico-rodriguez@users.noreply.github.com>
2019-05-18 22:50:07 -0300

    Refactored some logic inside forwardpropagation and forwardpropagation_z.

--- ejercicio8/Utils.py
+++ ejercicio8/Utils.py
@@ -27,3 +27,30 @@ Aplicar la derivada de la función tangente hiperbolica a cada elemento de un ar
 """
 def d_tanh(arr):
 	return 1 - (np.tanh(arr) ** 2)
+
+
+"""
+Agregar un 1 en la primera posición del arreglo (o si es una matriz, en la primera fila).
+"""
+def extend_array(arr):
+	if arr.ndim == 1:
+		arr = np.vstack([1, arr])
+	elif arr.ndim == 2:
+		arr = np.vstack([np.ones(arr.shape[1]), arr])
+	else:
+		raise Exception('Utils.py: invalid array dimension in extend_array')
+	return arr
+
+
+"""
+Redimensionar el arreglo n-dimensional. Si es un vector, convertirlo en un arreglo n-dimensional de una columna;
+si es un arreglo de dos dimentiones, trasponerlo.
+"""
+def reshape_array(arr):
+	if arr.ndim == 1:
+		arr = arr.reshape(len(arr), 1)
+	elif arr.ndim == 2:
+		arr = arr.T
+	else:
+		raise Exception('Utils.py: invalid array dimension in reshape_array')
+	return arr
